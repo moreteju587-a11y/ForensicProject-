b0418fa43eca26d3000de3b463796fd7a3be320c
moreteju587-a11y <moreteju587@gmail.com>
2026-07-19 15:46:07 +0530

    Fix Railway deployment memory issue

--- image_generator.py
+++ image_generator.py
@@ -4,30 +4,48 @@ from diffusers import DPMSolverMultistepScheduler
 import os
 import uuid
 
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 model_id = "SG161222/Realistic_Vision_V6.0_B1_noVAE"
 
-print("Loading Realistic Vision model...")
 
-pipe = StableDiffusionPipeline.from_pretrained(
-    model_id,
-    torch_dtype=torch.float16 if device == "cuda" else torch.float32,
-    safety_checker=None,
-    feature_extractor=None
-)
+pipe = None
 
-pipe = pipe.to(device)
-pipe.scheduler = DPMSolverMultistepScheduler.from_config(
-    pipe.scheduler.config
-)
 
-pipe.enable_attention_slicing()
-pipe.enable_vae_slicing()
+def load_model():
 
-print("Model loaded!")
+    global pipe
+
+    if pipe is None:
+
+        print("Loading Realistic Vision model...")
+
+        pipe = StableDiffusionPipeline.from_pretrained(
+            model_id,
+            torch_dtype=torch.float32,
+            safety_checker=None,
+            feature_extractor=None
+        )
+
+        pipe = pipe.to("cpu")
+
+        pipe.scheduler = DPMSolverMultistepScheduler.from_config(
+            pipe.scheduler.config
+        )
+
+        pipe.enable_attention_slicing()
+        pipe.enable_vae_slicing()
+
+        print("Model loaded!")
+
+    return pipe
+
+
 
 def generate_face(prompt):
+
+    model = load_model()
 
     negative_prompt = (
         "blurry, low quality, worst quality, deformed face, "
@@ -35,7 +53,7 @@ def generate_face(prompt):
         "cropped, watermark, text, cartoon, painting"
     )
 
-    image = pipe(
+    image = model(
         prompt=prompt,
         negative_prompt=negative_prompt,
         num_inference_steps=20,
@@ -44,14 +62,23 @@ def generate_face(prompt):
         height=768
     ).images[0]
 
-    output_folder = os.path.join("static", "generated_faces")
+
+    output_folder = os.path.join(
+        "static",
+        "generated_faces"
+    )
+
     os.makedirs(output_folder, exist_ok=True)
+
 
     filename = f"{uuid.uuid4().hex}.png"
 
-    output_path = os.path.join(output_folder, filename)
+    output_path = os.path.join(
+        output_folder,
+        filename
+    )
+
 
     image.save(output_path)
 
     return output_path
-    
